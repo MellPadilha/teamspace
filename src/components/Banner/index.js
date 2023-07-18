584330c3af11d8385e26df901829ec9bda4028c7
Méllanie Kassim Padilha Taveira <mellanie.taveira@cslr-wga.com.br>
2023-07-18 14:52:09 -0300

    PGW

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/assets/banner.png" alt="Banner"/>
+        </header>
+        
+    );
+}
+
+export default Banner
